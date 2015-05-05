8d14475ba8a0fbf11bae5868549ab42e2695fd1c
anetegithub <anete.anetes@gmail.com>
2015-05-05 17:26:01 +0500

    Cordova ft hw

---
+++ Ionic Blank Template1/scripts/map.js
@@ -0,0 +1,27 @@
+﻿function mapnew() {
+    var map = new Object();
+
+    map.draw = function (tileid,x,y) {
+        if (tileid<0 || tileid>3)
+            return;
+        
+        var canvas = document.getElementById("map"),
+            content = canvas.getContext('2d'),
+            pic = new Image();
+
+        
+        switch (tileid) {
+            case 0: { pic.src = "../images/Tiles/alienPink.png"; break; }
+            case 1: { pic.src = "../images/Tiles/tileDirt.png"; break; }
+            case 2: { pic.src = "../images/Tiles/rockDirt_moss3.png"; break; }
+            case 3: { pic.src = "../images/Tiles/tileDirt_full.png"; break; }
+        }
+
+        pic.onload = function () {    // when image loaded
+            //content.strokeText('wtf suka',x,y-5)
+            content.drawImage(pic, x, y);
+        }        
+    };
+
+    return map;
+}
